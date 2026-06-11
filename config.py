f5debaeb485c23dfe1dace87bbf7b26dc4762853
sunilsahani7520-afk <sunilsahani7520@gmail.com>
2026-06-11 08:37:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 class Config:
     # ─── BOT SETTINGS ─────────────────────────────────────────────────────
-    BOT_TOKEN = "BOT_TOKEN"        # Get from @BotFather
-    BOT_NAME  = "ARAFAT FILE STORE"          # Your bot's display name
+    BOT_TOKEN = "8835991666:AAHnQXcikJcSw_vWK4xg2jM27kjVHxLkWxI"        # Get from @BotFather
+    BOT_NAME  = "FILE STORE"          # Your bot's display name
 
     # ─── ADMIN SETTINGS ───────────────────────────────────────────────────
     ADMIN_IDS = [
-        "6337373375",    # Your Telegram User ID (as string)
+        "7938671247",    # Your Telegram User ID (as string)
     ]
 
     # ─── OWNER CONTACT ────────────────────────────────────────────────────
-    OWNER_USERNAME = "ARAFAT_CONTACT"         # Without the @ symbol
+    OWNER_USERNAME = "THE_DEVLOPER_KING"         # Without the @ symbol
 
     # ─── COIN SETTINGS ────────────────────────────────────────────────────
     REFERRAL_COINS     = 1   # Coins referrer earns per successful referral
